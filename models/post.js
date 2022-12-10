4bc6697c598f4c832dbcf0d9354cf7af694db0d1
Kevin Souza <kevinxsouza@gmail.com>
2022-12-10 15:56:55 -0500

    timestamp added

--- models/post.js
+++ models/post.js
@@ -7,7 +7,7 @@ const postSchema = new mongoose.Schema ({
   linkURL: String,
   likes: Array,
   comments: [{user: String, comment: String}]
-})
+} {timestamp: true})
 
 const Post = mongoose.model('Post', postSchema)
 
